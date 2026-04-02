b0d78a1eaee3d8edd7c2057e1edef423121c77d4
Nono Heryana <nonoheryana@gmail.com>
2026-04-02 09:36:46 +0700

    Delete config directory

--- config/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => getenv('APP_NAME') ?: 'ADIPATI',
-    'env' => getenv('APP_ENV') ?: 'production',
-    'debug' => filter_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN) ?: false,
-    'url' => getenv('APP_URL') ?: 'http://localhost',
-    'timezone' => getenv('APP_TIMEZONE') ?: 'UTC',
-];
